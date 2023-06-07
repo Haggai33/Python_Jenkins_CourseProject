69012663dc0de1920fe16bd5c0fb310eaa77e8fa
Administrator <chagai33@gmail..com>
2023-06-07 20:42:59 +0300

    env fix

--- clean_environment.py
+++ clean_environment.py
@@ -2,13 +2,13 @@ import requests
 
 try:
     requests.get('http://127.0.0.1:5000/stop_server')
-    # print("Rest server stop")
+
 except requests.exceptions.RequestException as e:
     print(f'Rest server is NOT responding')
 
     try:
         requests.get('http://127.0.0.1:5001/stop_server')
-        print("Web_app server stop")
+
     except requests.exceptions.RequestException as e:
         print(f'Web_app is NOT responding')
 
